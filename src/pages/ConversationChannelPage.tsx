b43c59b1248cd70f8b4b26e2b498c969b45e64ff
hoangat <hoangat4848@gmail.com>
2022-11-26 20:29:58 +0700

    Move socket event listener up to ConversationPage

--- src/pages/ConversationChannelPage.tsx
+++ src/pages/ConversationChannelPage.tsx
@@ -3,11 +3,9 @@ import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import MessagePanel from "../components/messages/MessagePanel";
 import { AppDispatch } from "../store";
-import { updateConversation } from "../store/slices/conversationSlice";
-import { addMessage, fetchMessagesThunk } from "../store/slices/messageSlice";
+import { fetchMessagesThunk } from "../store/slices/messageSlice";
 import { SocketContext } from "../utils/context/SocketContext";
 import { StyledConversationChannelPage } from "../utils/styles";
-import { MessageEventPayload } from "../utils/types";
 
 const ConversationChannelPage = () => {
   const { id } = useParams();
@@ -19,19 +17,6 @@ const ConversationChannelPage = () => {
     const conversationId = parseInt(id!);
     dispatch(fetchMessagesThunk(conversationId));
   }, [id, dispatch]);
-
-  useEffect(() => {
-    socket.emit("onClientConnect", { conversationId: parseInt(id!) });
-    socket.on("onMessage", (payload: MessageEventPayload) => {
-      dispatch(addMessage(payload));
-      dispatch(updateConversation(payload.conversation));
-    });
-
-    return () => {
-      socket.off("connected");
-      socket.off("onMessage");
-    };
-  }, [socket, dispatch]);
 
   const sendTypingStatus = () => {
     socket.emit("onUserTyping", { conversationId: id });
--- src/pages/ConversationPage.tsx
+++ src/pages/ConversationPage.tsx
@@ -1,15 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet, useParams } from "react-router-dom";
 import ConversationSidebar from "../components/conversations/ConversationSidebar";
 import { AppDispatch } from "../store";
-import { fetchConversationsThunk } from "../store/slices/conversationSlice";
-import { getConversations } from "../utils/api";
+import {
+  fetchConversationsThunk,
+  updateConversation,
+} from "../store/slices/conversationSlice";
+import { addMessage } from "../store/slices/messageSlice";
+import { SocketContext } from "../utils/context/SocketContext";
 import { Page } from "../utils/styles";
-import { Conversation } from "../utils/types";
+import { MessageEventPayload } from "../utils/types";
 
 const ConversationPage = () => {
+  const { id } = useParams();
   const dispatch = useDispatch<AppDispatch>();
+  const socket = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.emit("onClientConnect", { conversationId: parseInt(id!) });
+    socket.on("onMessage", (payload: MessageEventPayload) => {
+      dispatch(addMessage(payload));
+      dispatch(updateConversation(payload.conversation));
+    });
+
+    return () => {
+      socket.off("connected");
+      socket.off("onMessage");
+    };
+  }, [socket, dispatch]);
 
   useEffect(() => {
     dispatch(fetchConversationsThunk());
