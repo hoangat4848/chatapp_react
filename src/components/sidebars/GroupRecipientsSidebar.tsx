9bec09eb102f2f25f469a52a8f6549557073f15b
hoangat <hoangat4848@gmail.com>
2022-12-16 03:16:26 +0700

    Polling websocket to receive online & offline users

--- src/components/sidebars/GroupRecipientsSidebar.tsx
+++ src/components/sidebars/GroupRecipientsSidebar.tsx
@@ -1,9 +1,10 @@
 import { PeopleGroup } from "akar-icons";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { RootState } from "../../store";
 import { selectGroupById } from "../../store/slices/groupSlice";
+import { SocketContext } from "../../utils/context/SocketContext";
 import {
   GroupRecipientsSidebarHeader,
   GroupRecipientsSidebarItem,
@@ -11,12 +12,36 @@ import {
   MessageItemAvatar,
   StyledGroupRecipientsSidebar,
 } from "../../utils/styles";
+import { User } from "../../utils/types";
 
 const GroupRecipientsSidebar = () => {
-  const { id } = useParams();
+  const { id: groupId } = useParams();
   const group = useSelector((state: RootState) =>
-    selectGroupById(state, parseInt(id!))
+    selectGroupById(state, parseInt(groupId!))
   );
+  const socket = useContext(SocketContext);
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([]);
+  const [offlineUsers, setOfflineUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    socket.emit("getOnlineGroupUsers", { groupId: parseInt(groupId!) });
+    const interval = setInterval(() => {
+      console.log(`Pinging Group${groupId}`);
+      socket.emit("getOnlineGroupUsers", { groupId: parseInt(groupId!) });
+    }, 10000);
+
+    socket.on("onlineGroupUsersReceived", (payload) => {
+      console.log("received payload for online users");
+      console.log(payload);
+      setOnlineUsers(payload.onlineUsers);
+      setOfflineUsers(payload.offlineUsers);
+    });
+    return () => {
+      console.log("Clearing interval for groupRecipientsSidebar");
+      clearInterval(interval);
+      socket.off();
+    };
+  }, [groupId, socket]);
 
   return (
     <StyledGroupRecipientsSidebar>
@@ -25,8 +50,16 @@ const GroupRecipientsSidebar = () => {
         <PeopleGroup size={24} strokeWidth={2} />
       </GroupRecipientsSidebarHeader>
       <GroupRecipientsSidebarItemContainer>
-        {group?.users.map((user) => (
-          <GroupRecipientsSidebarItem>
+        <p>Online Users</p>
+        {onlineUsers.map((user) => (
+          <GroupRecipientsSidebarItem key={user.id}>
+            <MessageItemAvatar />
+            {user.email}
+          </GroupRecipientsSidebarItem>
+        ))}
+        <p>Offline Users</p>
+        {offlineUsers.map((user) => (
+          <GroupRecipientsSidebarItem key={user.id}>
             <MessageItemAvatar />
             {user.email}
           </GroupRecipientsSidebarItem>
