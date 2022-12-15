78f52ee7058b6b03d4e36fd4fe6c8871eda494d8
hoangat <hoangat4848@gmail.com>
2022-12-16 03:17:03 +0700

    Truncate group title to max length

--- src/components/groups/GroupSidebarItem.tsx
+++ src/components/groups/GroupSidebarItem.tsx
@@ -8,9 +8,30 @@ type Props = {
 };
 const GroupSidebarItem = ({ group }: Props) => {
   const navigate = useNavigate();
+  const MAX_TITLE_LENGTH = 20;
+  const MAX_MESSAGE_LENGTH = 20;
+
   const content = group.lastMessageSent?.content;
-  const displayContent =
-    content && content.length > 10 ? content.substring(0, 20) + "..." : content;
+  const displayGroupLastMessageSent =
+    content && content.length > MAX_MESSAGE_LENGTH
+      ? content.substring(0, MAX_MESSAGE_LENGTH).concat("...")
+      : content;
+
+  const getTransformedTitle = () => {
+    const groupTitle = group.title;
+    if (!groupTitle) {
+      const usersToString = group.users
+        .map((user) => user.firstName)
+        .join(", ");
+      return usersToString.length > MAX_TITLE_LENGTH
+        ? usersToString.substring(0, MAX_TITLE_LENGTH).concat("...")
+        : usersToString;
+    }
+
+    return groupTitle.length > MAX_TITLE_LENGTH
+      ? groupTitle.substring(0, MAX_TITLE_LENGTH).concat("...")
+      : groupTitle;
+  };
 
   return (
     <StyledConversationSidebarItem
@@ -18,10 +39,10 @@ const GroupSidebarItem = ({ group }: Props) => {
     >
       <div className={styles.conversationAvatar}></div>
       <div>
-        <span className={styles.conversationName}>
-          {group.title ?? "Group"}
+        <span className={styles.conversationName}>{getTransformedTitle()}</span>
+        <span className={styles.conversationLastMessage}>
+          {displayGroupLastMessageSent}
         </span>
-        <span className={styles.conversationLastMessage}>{displayContent}</span>
       </div>
     </StyledConversationSidebarItem>
   );
